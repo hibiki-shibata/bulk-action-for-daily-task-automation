ae5e47c5c9e5e19a23254b2a969aaf4d659242c4
hibiki-shibata <hibikishibata1853@gmail.com>
2025-08-15 20:36:33 +0900

    edited resource name && readme.md

--- resource/globalConfig.ts
+++ resource/globalConfig.ts
@@ -1,8 +1,8 @@
 export const globalConfig  = {
-  requestUriPath: "https://example-restautnt-management/open/",
+  requestUriPath: "https://example-restautnt-management/open/", // Base URI for the request
   requestMethod: "PUT",
-  csvPath: "./resource/venueID.csv", // Path to the CSV that will be used in the request
-  jsonPath: "./resource/requestBody.json" // Path to the JSON that will be used in the request
+  csvPath: "./resource/targetID-example.csv", // Path to the CSV that will be used in the request
+  jsonPath: "./resource/requestBody-example.json" // Path to the JSON that will be used in the request
 }
 
 
