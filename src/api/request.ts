19c60ff33f5895c9a6d792dd6aad8a32c263a499
hibiki-shibata <hibikishibata1853@gmail.com>
2025-08-15 23:55:20 +0900

    csvRepo make constructor private

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,4 +1,4 @@
-import { globalConfig }from "../../resource/globalConfig.js";
+import { globalConfig }from "../../resource/globalConfig.js"
 
 export async function sendRequest(accessToken: string, requestURI: string, requestBodyJson: Object): Promise<boolean> {
     const response: Response = await fetch(requestURI, {
@@ -10,11 +10,11 @@ export async function sendRequest(accessToken: string, requestURI: string, reque
         body: JSON.stringify(requestBodyJson),
         signal: AbortSignal.timeout(5000)
     }).catch(error => {
-        throw Error(`Network error: [${error.message}] for URI: [${requestURI}]`);
-    });
+        throw Error(`Network error: [${error.message}] for URI: [${requestURI}]`)
+    })
 
-    if (!response.ok) throw Error(`Request failed with status ${response.status} for URI: ${requestURI}`);
-    console.log(`Request sent successfully for URI: ${requestURI}`);
+    if (!response.ok) throw Error(`Request failed with status ${response.status} for URI: ${requestURI}`)
+    console.log(`Request sent successfully for URI: ${requestURI}`)
 
     return response.ok
 }
--- src/repository/csvRepository.ts
+++ src/repository/csvRepository.ts
@@ -1,41 +1,4 @@
-// import { globalConfig } from "../../resource/globalConfig.js";
 import { fileReader } from '../util/fileReader.js'
-
-// export async function csvRepository() {
-//     let listOfVenueIdsToUpdate: any[] = []
-//     let countLinesOfCsvRows = 1
-
-//     const rawCsvData = await fileReader(globalConfig.csvPath)
-//     const csvDataRows = rawCsvData.split('\n')
-
-//     csvDataRows.forEach((rowOfCsv: any) => {
-//         const validRow = csvDataFormatValidator(rowOfCsv)
-//         if (!validRow) {
-//             console.log(`Invalid CSV format at line ${countLinesOfCsvRows}: "${rowOfCsv}". Please ensure the CSV file does not contain empty lines or multiple columns.`);
-//         } else listOfVenueIdsToUpdate.push(validRow)
-
-//         countLinesOfCsvRows += 1
-//     }
-//     )
-
-//     if (listOfVenueIdsToUpdate.length === 0) throw Error("The CSV file is empty or does not contain valid venue IDs.");
-
-//     return listOfVenueIdsToUpdate
-// }
-
-
-//Spec1: Does not allow empty lines in the CSV.
-//Spec2: Does not allow multiple columns in the CSV.
-// function csvDataFormatValidator(rowOfCsv: string) {
-//     let trimedRowOfCsvData = rowOfCsv.trim()
-//     const isEmptyRow = trimedRowOfCsvData === ""
-//     const hasMultipleColumns = trimedRowOfCsvData.includes(",")
-
-//     if (isEmptyRow || hasMultipleColumns) {
-//         return false
-//     } else return trimedRowOfCsvData
-
-// }
 
 
 interface ICsvRepository {
@@ -48,14 +11,16 @@ export class CsvRepository implements ICsvRepository {
 
     public static async getInstanceAndloadCsvFrom(csvPath: string) {
         this.rawCsvData = await fileReader(csvPath)
-        if (!this.rawCsvData) throw Error("The CSV file is empty or does not contain valid data.");
-
+        if (!this.rawCsvData) throw Error("The CSV file is empty or does not contain valid data.")
         return new CsvRepository()
     }
 
-    public async getAllDataInColumnOf(columnName: string): Promise<string[]> {
-        if (!CsvRepository.rawCsvData) throw Error("CSV data is not loaded. Please call getInstanceAndloadCsvFrom first.");
+    private constructor() {
+      if (!CsvRepository.rawCsvData)throw new Error("CSV data is not loaded. Please call getInstanceAndloadCsvFrom first.")
+        
+    }
 
+    public async getAllDataInColumnOf(columnName: string): Promise<string[]> {
         var listOfTargetIdsToUse: any[] = []
         var countLinesOfCsvRows: number = 1
 
