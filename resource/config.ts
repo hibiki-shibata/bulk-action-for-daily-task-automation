0b5a547f7a5e06ef5cd875477f98cc6729af2540
Hibiki Shibata <102745641+hibiki-shibata@users.noreply.github.com>
2025-08-20 02:20:33 +0900

    Update config.ts

--- resource/config.ts
+++ resource/config.ts
@@ -8,42 +8,28 @@ import { configType } from "../src/type/config.Type.js"; export const config: co
 
 
 
-
-
   // A) Request Method.
   request_method: "PUT",
-
 
   // B) Request URI.
   //        Optionally, use <Your Column Name> as a placeholder for replacing it with data in the CSV column below.
   request_uri: "https://example-restautnt-management/open/[Customer ID]/customerID/[Venue Address]/venueaddress/[Contact]/contact",
 
-
-
   // C) Primary Column name in CSV file.
-  //        Data in this column will replace [<Your Column Name>].
+  //        Data in this column will replace the placeholders(e.g.[Customer ID]) in URI / Request Body.
   base_csv_column_name: "Customer ID",
 
-
-
   // D) Optional‼️ : Additional CSV column names.
-  //                      If you want to use additional columns in the CSV file, specify them here.
-  //                      You can specify up to 10 additional columns.
+  //                      If you want to use additional columns in the CSV file, specify them here. (Up to 10 columns)
   optional_csv_column_name_1: "Customer ID",
   optional_csv_column_name_2: "Venue Address",
 
-
-
-  // E) Paths to the resource files.
-  csv_file_path: "./resource/custom-values.csv", // Define your custom values.
+  // E) Paths to your resource files.
+  csv_file_path: "./resource/custom-values.csv", // Define your custom data.
   json_file_path: "./resource/requestBody-config.json", // Define your request body.
-
-
 
   // F) Access Token header name.
   security_header_name: "Authorization",
-
-
 
 
 
