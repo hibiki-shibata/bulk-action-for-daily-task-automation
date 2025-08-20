ab33267b7cd341f5073e39a2f58d230c581a110a
hibiki-shibata <hibikishibata1853@gmail.com>
2025-08-20 18:18:21 +0900

    reading token file instead of command line

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 // This file is the entry point for the application
 // Run this file to start the application
-import { askAcccessTokenInCli } from './util/cli.js';
+import { AuthorizationHeaderAndBodyJsonService } from './service/authorizationHeaderAndBodyJsonService.js'
+import { fileReader } from './/util/fileReader.js';
+
+
 
 
 async function main(): Promise<void> {
-    await askAcccessTokenInCli()
+    console.log(`=========================================================================\n\n⚠️ Do not forget to update your Access Token in access-token.txt file ⚠️\n\n=========================================================================`)
+    console.log(`STARTING...🤖`)
+    AuthorizationHeaderAndBodyJsonService.setAccessToken(fileReader('./resource/access-token.txt')).executeBulkAction()
 }
 
 
 main().catch(error => {
-    console.error('An error occurred:', error);
+    console.error('An error occurred:', error)
+    process.exit(1)
 }
 )
 
